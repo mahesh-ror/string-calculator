c28a26d348e5c41d08b20fd766e6a33d6fca8ff0
mahesh-ror <patidarmahesh0045@gmail.com>
2025-04-03 16:22:08 +0530

    cover scenerio for custom delimiters

--- lib/calculator.rb
+++ lib/calculator.rb
@@ -3,6 +3,18 @@
 class Calculator
   def self.add(numbers)
     return 0 if numbers.empty?
-    numbers.split(/,|\n/).map(&:to_i).sum
+    @string_number = numbers
+    numbers.split(/#{delimiter}|\n/).map(&:to_i).sum
+  end
+
+  private
+
+  def self.delimiter
+    str = ','
+    if @string_number.start_with?('//')
+      str, _numbers = @string_number.split("\n", 2)
+      str = str[2..]
+    end
+    str
   end
 end
